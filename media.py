7156ce235b5a9c0ab047c2bc11035b0a1792e68d
David Maydan <dmaydan2@gmail.com>
2018-06-09 11:53:48 -0400

    Fix for PEP8

--- media.py
+++ media.py
@@ -1,9 +1,9 @@
 class Movie:
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
-		'''
-		Instantiates a Movie object with a title,
-		poster image, and youtube trailer as attributes
-		'''
-		self.title = title
-		self.poster_image_url = poster_image_url
-		self.trailer_youtube_url = trailer_youtube_url
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        '''
+        Instantiates a Movie object with a title,
+        poster image, and youtube trailer as attributes
+        '''
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
